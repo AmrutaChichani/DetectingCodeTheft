3cdd62d1b995377407e6d7bba309d362b5ef15a0
Amruta Chichani <amruta.chichani@clairvoyantsoft.com>
2022-09-16 17:00:08 +0530

    Adding for Demo purpose

---
+++ codeTheft/src/main/java/com/paypal/CreationPathController.java
@@ -0,0 +1,12 @@
+package com.paypal;
+
+public class CreationPathController {
+    String purpose ="DEMO FILE - FOR CODE THEFT DETECTION";
+
+    CreationPathController() {
+        print();
+    }
+    public void print() {
+        System.out.println("https://artifact.internal.shutterfly.com");
+    }
+}
